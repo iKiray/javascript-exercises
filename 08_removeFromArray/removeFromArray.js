e1bc6914fb59a8aabe7d95af701abc0c8fa0d244
iKiray <exkiray@gmail.com>
2025-11-25 23:51:12 -0500

    Finished 08 exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+   const newArray = array.filter(item => !items.includes(item));
+
+    return newArray;
 };
 
 // Do not edit below this line
