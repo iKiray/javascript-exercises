0e62b39b3228eab6880554afd5894786464d8061
iKiray <exkiray@gmail.com>
2025-11-26 23:24:36 -0500

    Finished 09 exercise files

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumAll = 0
 
+    if (Number.isInteger(num1) !== true || Number.isInteger(num2) !== true) {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sumAll += i;
+    }
+
+    return sumAll;
 };
 
 // Do not edit below this line
