63ae476ea16a134ab76af55580ab60b159882fa7
iKiray <exkiray@gmail.com>
2025-11-27 00:07:10 -0500

    Finished 12 exercise files

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let multiSum = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    multiSum *= array[i];
+  }
+
+  return multiSum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } else if (num > 0) {
+    let factorial = 1;
+
+    for (let i = factorial; i <= num; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
