46e18c5dd938c828fb0b7e913a91b8309666bc4a
iKiray <exkiray@gmail.com>
2025-11-26 23:51:08 -0500

    Finished 11 exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conCel = (temp - 32) / (9/5);
+  
+  if (conCel === 0) {
+    return 0;
+  } else {
+    let fixed = conCel.toFixed(1);
+    return parseFloat(fixed);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conFah = temp * (9/5) + 32;
+
+  if (conFah === 0) {
+    return 0;
+  } else {
+    let fixed = conFah.toFixed(1);
+    return parseFloat(fixed);
+  }
+
 };
 
 // Do not edit below this line
