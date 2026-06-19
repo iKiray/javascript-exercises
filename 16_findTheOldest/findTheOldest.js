77fc5c14d4659fb586e2db352453f3ff2f44a094
iKiray <exkiray@gmail.com>
2026-06-18 23:11:56 -0400

    Complete findTheOldest exercise - all tests passing

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currYear = new Date().getFullYear();
+        const age = (person.yearOfDeath || currYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currYear) - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
