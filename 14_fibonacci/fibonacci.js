3a65dd7b040f6bfc1c20a38633ec5912db173cd7
iKiray <exkiray@gmail.com>
2026-06-18 21:56:09 -0400

    Complete fibonacci exercise - all tests passing

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const numOnly = +num;
+    if (numOnly < 0) {
+        return "OOPS";
+    } else if (numOnly === 0) {
+        return 0;
+    } else {
+        let prev = 0;
+        let curr = 1;
+        for (let i = 0; i < numOnly - 1; i++) {
+            let next = prev + curr;
+            prev = curr;
+            curr = next;
+        };
+        return curr;
+    }
 };
 
 // Do not edit below this line
