6769464e64e2113dd186719e2bf1eccbddbae7e5
iKiray <exkiray@gmail.com>
2026-06-18 20:43:58 -0400

    Complete palindromes exercise - all tests passing

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanStr = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    const reverseStr = cleanStr.split("").reverse().join("");
 
+    if (reverseStr === cleanStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
